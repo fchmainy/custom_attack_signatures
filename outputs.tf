72fdbb46d401cf3258490b61b2ef75139bfaf813
Fouad Chmainy <fchmainy@yahoo.com>
2024-07-30 16:59:17 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "signatureId" {
 output "custom_attack_sig_id" {
   value = jsondecode(restapi_object.attack_signature.id)
 }
-
-output "custom_attack_sig_id" {
-  value = jsondecode(restapi_object.attack_signature.id)
-}

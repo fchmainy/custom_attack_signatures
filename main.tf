2660b54dd4095c92e30c384b3ece00bd7bac2cc7
Fouad Chmainy <fchmainy@yahoo.com>
2024-07-30 15:04:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,23 +28,7 @@ locals {
 
   })
 }
-resource "restapi_object" "login" {
-  provider  = restapi.before_login_endpoint
-  object_id = "authToken"
-  path      = "/mgmt/shared/authn/login"
-  data      = local.login
-}
 
-resource "restapi_object" "login2" {
-  provider       = restapi.after_login_endpoint
-  object_id      = "authToken"
-  path           = "/mgmt/shared/authn/login"
-  data           = local.login
-  update_method  = "PATCH"
-  destroy_method = "DELETE"
-  debug          = true
-
-}
 
 resource "restapi_object" "attack_signature" {
   provider  = restapi.after_login_endpoint
